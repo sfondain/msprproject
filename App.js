01c57edc8a23c200d33fdadd508e7fb1450819c7
sfondain <steeve.fondain@gmail.com>
2020-01-27 11:00:15 +0100

    Test first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.test}>J'ai le pire groupe</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  test: {
+    fontSize:10,
+    fontFamily:'monospace'
+  }
+});
